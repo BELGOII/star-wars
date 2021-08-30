6213f8a1378371a80b7bf5d223f48ca7d5120967
unknown <gigatsitsagi2@gmail.com>
2021-08-30 19:59:40 +0400

    second commit

--- StarWars/starwars.js
+++ StarWars/starwars.js
@@ -1,17 +1,28 @@
 const slideView = document.getElementById('slide-view')
-const slideEnd = (slides, clone)=> {
-    slides.style.left = '0';
-    slides.appendChild(clone);
-    console.log('HELP')
+const slideEnd = (slides) => {
+    slides.style.left = '0'
+    slides.appendChild(slides.children[0])
 }
-slideView.addEventListener('click', ()=>{
-    const slides = slideView.firstElementChild;
-    const firstSlide = slides.firstChild;
-//    const clone = firstSlide.cloneNode();
-   slides.removeChild(firstSlide);
-    // slides.style.left = '-900px';
-    // setTimeout(() => {
-    //     slideEnd(slides, clone)
-    // }, 750);
-    
+
+let isSlidable = true
+
+slideView.addEventListener('click', () => {
+    slide()
 })
+
+const slide = () => {
+    if (!isSlidable) return
+    isSlidable = false
+    const slides = slideView.firstElementChild
+    slides.style.transition = '750ms'
+    slides.style.left = '-80vw'
+    setTimeout(() => {
+        isSlidable = true
+        slides.style.transition = '0ms'
+        slideEnd(slides)
+    }, 750)
+}
+
+setInterval(() => {
+    slide()
+}, 2000)
